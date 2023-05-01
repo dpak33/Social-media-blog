605c3fefbd06a7de6df7627cdbe9423565e2368d
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-01 14:32:13 +0100

    y

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -1,4 +1,6 @@
 import Blog from "../model/Blog.js";
+import User from "../model/User.js";
+import mongoose from "mongoose";
 
 export const getAllBlogs = async (req, res, next) => {
     let blogs;
@@ -16,14 +18,31 @@ export const getAllBlogs = async (req, res, next) => {
 export const addBlog = async (req, res, next) => {
     const { title, description, image, user } = req.body;
 
+    let existingUser;
+    try {
+        existingUser = await User.findById(user);
+    } catch (err) {
+        return console.log(err);
+    };
+
+    if (!existingUser) {
+        return res.status(400).json({ message: "Unable to find user with this ID" })
+    }
+
     const blog = new Blog({
         title, description, image, user,
     });
 
     try {
-        await blog.save();
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await blog.save({ session });
+        existingUser.blogs.push(blog);
+        await existingUser.save({ session });
+        await session.commitTransaction();
     } catch (err) {
-        return console.log(err);
+        console.log(err);
+        return res.status(500).json({ message: err })
     }
     return res.status(200).json({ blog })
 };
@@ -68,7 +87,10 @@ export const deleteBlog = async (req, res, next) => {
 
     let blog;
     try {
-        blog = await Blog.findByIdAndRemove(id);
+        blog = await Blog.findByIdAndRemove(id).populate("user");
+        //deleting blog from user array below. We then have to save the results to ensure the changes are conveyed to DB
+        await blog.user.blogs.pull(blog);
+        await blog.user.save();
     } catch (err) {
         console.log(err);
     }
