498893b835910ee11f44c5107bdcef9cb82c2693
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-01 12:04:51 +0100

    y

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -27,3 +27,23 @@ export const addBlog = async (req, res, next) => {
     }
     return res.status(200).json({ blog })
 };
+
+export const updateBlog = async (req, res, next) => {
+    const { title, description } = req.body;
+    const blogId = req.params.id;
+    let blog;
+
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description,
+        });
+    } catch (err) {
+        return console.log(err)
+    } if (!blog) {
+        return res.status(500).json({
+            message: "Unable to update blog!"
+        })
+    }
+    return res.status(200).json({ blog });
+};
