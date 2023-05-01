2079bd16a4fb3f2895194e6663ffa7dd9a7248cc
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-01 13:45:17 +0100

    y

--- backend/model/User.js
+++ backend/model/User.js
@@ -16,7 +16,9 @@ const userSchema = new Schema({
         type: String,
         required: true,
         minlength: 6
-    }
+    },
+    blogs: [{ type: mongoose.Types.ObjectId, ref: "Blog", required: true }],
 });
+
 export default mongoose.model("User", userSchema);
 //users to indicate that in MongoDb it will be stored as lower case in MongoDb.
