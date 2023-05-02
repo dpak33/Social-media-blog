4651e56737ef4d4a27d4094e9584201dd76cb9f8
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-02 12:58:25 +0100

    y

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Box, Button, Tab, Tabs } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 const theme = createTheme({
     status: {
@@ -20,21 +21,32 @@ const theme = createTheme({
 
 
 const Header = () => {
-
+    const [value, setValue] = useState();
 
     return (
-        <AppBar sx={{ background: 'linear-gradient(90deg, rgba(226,236,238,1) 4%, rgba(2,0,36,1) 98%)' }}>
+        <AppBar
+            position="sticky"
+            sx={{ background: 'linear-gradient(90deg, rgba(226,236,238,1) 4%, rgba(2,0,36,1) 98%)' }}>
             <Toolbar>
                 <Typography variant="h2" color="black" fontFamily={"Helvetica Neue"}>
                     Blog App
                 </Typography>
-                <Box marginLeft={2}>
+                <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
+                    <Tabs value={value} onChange={(e, val) => setValue(val)}>
+                        <Tab label="All blogs" />
+                        <Tab label="My blogs" />
+                    </Tabs>
+                </Box>
+                <Box marginLeft="auto">
                     <ThemeProvider theme={theme}>
-                        <Button color="neutral" variant="contained">
+                        <Button color="neutral" variant="contained" sx={{ mr: 1 }}>
                             <Typography color="black">Login</Typography>
                         </Button>
-                        <Button color="neutral" variant="contained">
+                        <Button color="neutral" variant="contained" sx={{ mr: 1 }}>
                             <Typography color="black">Signup</Typography>
+                        </Button>
+                        <Button color="neutral" variant="contained">
+                            <Typography color="black">Logout</Typography>
                         </Button>
                     </ThemeProvider>
                 </Box>
