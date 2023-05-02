513a1814fa1a26b69ddc495153f3a04594d3bd2c
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-02 11:16:17 +0100

    y

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    status: {
+        danger: '#e53e3e',
+    },
+    palette: {
+        primary: {
+            main: '#0971f1',
+            darker: '#053e85',
+        },
+        neutral: {
+            main: '#64748B',
+            contrastText: '#fff',
+        },
+    },
+});
+
 
 const Header = () => {
+
+
     return (
-        <AppBar>
+        <AppBar sx={{ background: 'linear-gradient(90deg, rgba(226,236,238,1) 4%, rgba(2,0,36,1) 98%)' }}>
             <Toolbar>
-                <Typography variant="h4">
+                <Typography variant="h2" color="black" fontFamily={"Helvetica Neue"}>
                     Blog App
                 </Typography>
+                <Box marginLeft={2}>
+                    <ThemeProvider theme={theme}>
+                        <Button color="neutral" variant="contained">
+                            <Typography color="black">Login</Typography>
+                        </Button>
+                        <Button color="neutral" variant="contained">
+                            <Typography color="black">Signup</Typography>
+                        </Button>
+                    </ThemeProvider>
+                </Box>
             </Toolbar>
 
         </AppBar>
